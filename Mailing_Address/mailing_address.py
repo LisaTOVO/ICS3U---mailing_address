604896c00ad681025377ee1bdd6462ab8a03ed37
LisaTOVO <Jie.Tan21@ycdsbk12.ca>
2020-03-25 18:20:36 -0400

    mailing_address

---
+++ Mailing_Address/mailing_address.py
@@ -0,0 +1,9 @@
+name_own = "\nLISA TAN\n"
+address_own = "26 YELLOW BIRCH CRESCENT\n"
+postal_code_own = "RICHMOND HILL ON L4B 3R4\n"
+name_other = "UNDEFIND\n"
+adress_other = "8101 LESLIE ST\n"
+postal_code_other = "Thornhill, ON L3T 7P4\n"
+space = "                           "
+print(name_own + address_own + postal_code_own)
+print(space + name_other + space + adress_other + space + postal_code_other) 
